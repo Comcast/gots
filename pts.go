06c4de928db059118bc88c8a3db0855a045632bc
Mike Reedell <mike@reedell.com>
2018-04-17 21:57:27 -0600

    Fixing an off-by-one error in handling PTS addition that resulted in values >2^33.
    - The previous PTS addition code would subtract 2^33  - 1 from any PTS addition result above that
    value so the final result would be a valid PTS value.  Instead, the total length of the PTS
    timeline (2^33) should have been substracted.

--- pts.go
+++ pts.go
@@ -34,15 +34,18 @@ const (
 	PTS_DTS_INDICATOR_ONLY_PTS = 2 // 10
 	PTS_DTS_INDICATOR_NONE     = 0 // 00
 
-	// MaxPts is the highest value the PTS can hold before it rolls over, since its a 33 bit timestamp.
-	MaxPts = 8589934591 // 2^33 - 1
+	// MaxPtsValue is the highest value the PTS can hold before it rolls over, since its a 33 bit timestamp.
+	MaxPtsValue = 8589934591 // 2^33 - 1
+
+	// MaxPtsTicks is the length of the complete PTS timeline.
+	MaxPtsTicks = 8589934592 // 2^33.
 
 	// Used as a sentinel values for algorithms working against PTS
 	PtsNegativeInfinity = PTS(math.MaxUint64 - 1) //18446744073709551614
 	PtsPositiveInfinity = PTS(math.MaxUint64)     //18446744073709551615
 	PtsClockRate        = 90000
 
-	// UpperPtsRolloverThreshold is the threshold for a rollover on the upper end, maxPts = 30 min
+	// UpperPtsRolloverThreshold is the threshold for a rollover on the upper end, MaxPtsValue - 30 min
 	UpperPtsRolloverThreshold = 8427934591
 	// LowerPtsRolloverThreshold is the threshold for a rollover on the lower end, 30 min
 	LowerPtsRolloverThreshold = 162000000
@@ -92,9 +95,9 @@ func (p PTS) RolledOver(other PTS) bool {
 func (p PTS) DurationFrom(from PTS) uint64 {
 	switch {
 	case p.RolledOver(from):
-		return uint64((MaxPts + 1 - from) + p)
+		return uint64((MaxPtsTicks - from) + p)
 	case from.RolledOver(p):
-		return uint64((MaxPts + 1 - p) + from)
+		return uint64((MaxPtsTicks - p) + from)
 	case p < from:
 		return uint64(from - p)
 	default:
@@ -105,8 +108,8 @@ func (p PTS) DurationFrom(from PTS) uint64 {
 // Add adds the two PTS times together and returns a new PTS
 func (p PTS) Add(x PTS) PTS {
 	result := p + x
-	if result > MaxPts {
-		result = result - MaxPts
+	if result > MaxPtsValue {
+		result = result - MaxPtsTicks
 	}
 	return PTS(result)
 }
