87baf22318d50529c0f6b52aabc51507554869ea
Aman Gupta <aman@tmm1.net>
2018-05-14 20:17:58 -0700

    psi: expose StreamTypeDescription()

--- psi/doc.go
+++ psi/doc.go
@@ -74,6 +74,7 @@ type PSI interface {
 // PmtStreamType is used to represent elementary steam type inside a PMT
 type PmtStreamType interface {
 	StreamType() uint8
+	StreamTypeDescription() string
 	IsStreamWherePresentationLagsEbp() bool
 	IsAudioContent() bool
 	IsVideoContent() bool
