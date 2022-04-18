d428003161e33120af7d0456ebf51569e99fb7b3
Neha Shanbhag <neha_shanbhag@comcast.com>
2022-04-17 23:19:42 -0600

    Added closing logic for 0x26 and 0x27

--- scte35/segmentationdescriptor.go
+++ scte35/segmentationdescriptor.go
@@ -159,6 +159,8 @@ func init() {
 		0x23: {0x22: segCloseEventID, 0x30: segCloseNormal, 0x34: segCloseNormal, 0x36: segCloseNormal, 0x3C: segCloseNormal, 0x44: segCloseNormal},
 		0x24: {0x20: segCloseNormal, 0x22: segCloseNormal, 0x24: segCloseNormal, 0x26: segCloseNormal, 0x30: segCloseNormal, 0x34: segCloseNormal, 0x36: segCloseNormal, 0x3C: segCloseNormal, 0x44: segCloseNormal},
 		0x25: {0x24: segCloseEventID, 0x30: segCloseNormal, 0x34: segCloseNormal, 0x36: segCloseNormal, 0x3C: segCloseNormal, 0x44: segCloseNormal},
+		0x26: {0x20: segCloseNormal, 0x22: segCloseNormal, 0x24: segCloseNormal, 0x26: segCloseNormal, 0x30: segCloseNormal, 0x34: segCloseNormal, 0x36: segCloseNormal, 0x3C: segCloseNormal, 0x44: segCloseNormal},
+		0x27: {0x26: segCloseEventID, 0x30: segCloseNormal, 0x34: segCloseNormal, 0x36: segCloseNormal, 0x3C: segCloseNormal, 0x44: segCloseNormal},
 		0x30: {0x30: segCloseNormal, 0x32: segCloseNormal},
 		0x31: {0x30: segCloseEventID},
 		0x32: {0x30: segCloseNormal, 0x32: segCloseNormal},
